56720fffd28931a14cf731ece9bee2f6b9b719ab
Jacob Mischka <jacob@mischka.me>
2021-10-16 07:28:16 -0500

    Add basic pagination to reservations list

---
+++ src/components/Paginator.svelte
@@ -0,0 +1,30 @@
+<div class="paginator">
+	<button type="button" disabled={pageNumber == 0} on:click={() => { pageNumber = pageNumber - 1; }}>
+		←
+	</button>
+
+	<label>
+		Page
+		<input type="number" value={pageNumber + 1} on:change={handlePageChange} />
+	</label>
+
+	<button type="button" disabled={!pagesRemaining} on:click={() => { pageNumber += 1; }}>
+		→
+	</button>
+</div>
+
+<script>
+	export let limit = 10, offset = 0;
+	export let pagesRemaining = true;
+
+	let pageNumber = offset / limit;
+
+	$: offset = pageNumber * limit;
+
+	function handlePageChange(event) {
+		const value = Number(event.target.value);
+		if (Number.isNaN(value) || value < 1) return;
+
+		pageNumber = value - 1;
+	}
+</script>
--- src/components/Reservations/Reservations.svelte
+++ src/components/Reservations/Reservations.svelte
@@ -11,18 +11,24 @@
 				<ListItem {reservation} />
 			{/each}
 		</ol>
+	{:else}
+		<i>None found.</i>
 	{/if}
+	<Paginator bind:limit bind:offset pagesRemaining={reservations.length >= limit} />
 </section>
 <script type="typescript">
 	import ListItem from './ListItem.svelte';
+	import Paginator from '../Paginator.svelte';
+
+	let limit = 1;
+	let offset = 0;
 
 	import { Reservation } from '../../types.js';
 	import { fetchReservations } from '../../utils.js';
 
 	let reservations: Reservation[] = [];
 
-	// TODO: Paginate
-	fetchReservations().then(r => {
+	$: fetchReservations({ limit, offset }).then(r => {
 		reservations = r;
 	});
 </script>
